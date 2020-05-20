b722625b55f9159c59e7afa2fedc6943347542b7
ryoyumo <yumo.after.fun@gmail.com>
2020-05-20 16:12:23 +0900

    change the class design

--- src/ofxTweeny.hpp
+++ src/ofxTweeny.hpp
@@ -36,44 +36,19 @@ static constexpr auto easeInElastic  = tweeny::easing::elasticIn;
 static constexpr auto easeOutElastic  = tweeny::easing::elasticOut;
 static constexpr auto easeInOutElastic  = tweeny::easing::elasticInOut;
 
-using Tween = tweeny::tween<float>;
-
-namespace detail{
-class TweenItem {
-public:
-    template<typename EaseFunc>
-    TweenItem(float* ptr,
-              float from,
-              float to,
-              float duration, EaseFunc f)
-    : ptr_{ptr}{
-        *ptr_ = from;
-        tween_ = Tween::from(from).to(to).during(duration).via(f);
-    }
-    
-    TweenItem(float* ptr, const Tween& tween)
-    : ptr_{ptr}{
-        tween_ = tween;
-    }
-    
-    void update(int step){
-        *ptr_ = tween_.step(step);
-    }
-
-    bool complete() const {
-        return tween_.progress() >= 1.0 ? true : false;
-    }
-    const float* ptr() const { return ptr_; }
-    
-private:
-    float* ptr_;
-    Tween tween_;
-
-};
-} // namespace detail
+template<typename T>
+using Tween = tweeny::tween<T>;
 
 class Tweener {
 public:
+    template<typename T>
+    static void add(T* ptr, const Tween<T>& tween)
+    {
+        remove(ptr);
+        auto item = std::make_shared<TweenItemWrapper<T>>(ptr, tween);
+        instance().items_.push_back(item);
+    }
+    
     template<typename EaseFunc=tweeny::easing::linearEasing>
     static void add(float* ptr,
                     float from,
@@ -81,27 +56,19 @@ public:
                     float duration,
                     EaseFunc f=linear)
     {
-        remove(ptr);
-        instance().items_.push_back(detail::TweenItem(ptr, from, to, duration, f));
-    }
-    
-    static void add(float* ptr, const Tween& tween)
-    {
-        remove(ptr);
-        instance().items_.push_back(detail::TweenItem(ptr, tween));
+        add(ptr, Tween<float>::from(from).to(to).during(duration).via(f));
     }
     
     
     template<typename EaseFunc=tweeny::easing::linearEasing>
-       static void add(glm::vec2* ptr,
-                       const glm::vec2& from,
-                       const glm::vec2& to,
-                       float duration,
-                       EaseFunc f=linear)
-       {
-           add(&ptr->x, from.x, to.x, duration, f);
-           add(&ptr->y, from.y, to.y, duration, f);
-       }
+    static void add(glm::vec2* ptr,
+                    const glm::vec2& from,
+                    const glm::vec2& to,
+                    float duration,
+                    EaseFunc f=linear)
+    {
+        add(ptr, Tween<glm::vec2>::from(from).to(to).during(duration).via(f));
+    }
     
     template<typename EaseFunc=tweeny::easing::linearEasing>
     static void add(glm::vec3* ptr,
@@ -110,9 +77,7 @@ public:
                     float duration,
                     EaseFunc f=linear)
     {
-        add(&ptr->x, from.x, to.x, duration, f);
-        add(&ptr->y, from.y, to.y, duration, f);
-        add(&ptr->z, from.z, to.z, duration, f);
+        add(ptr, Tween<glm::vec3>::from(from).to(to).during(duration).via(f));
     }
     
     template<typename EaseFunc=tweeny::easing::linearEasing>
@@ -121,17 +86,25 @@ public:
                     const ofFloatColor& to,
                     float duration,
                     EaseFunc f=linear){
-        add(&ptr->r, from.r, to.r, duration, f);
-        add(&ptr->g, from.g, to.g, duration, f);
-        add(&ptr->b, from.b, to.b, duration, f);
-        add(&ptr->a, from.a, to.a, duration, f);
+        add(ptr, Tween<ofFloatColor>::from(from).to(to).during(duration).via(f));
     }
     
     
-    static void remove(float* ptr){
+    template<typename T, typename EaseFunc=tweeny::easing::linearEasing>
+    static void add(T* ptr, const std::vector<T>& to, float duration, EaseFunc f=linear){
+        auto tween = Tween<T>::from(*ptr);
+        for(auto i = 0; i < to.size(); ++i){
+            tween.to(to[i]).during(duration).via(f);
+        }
+        add(ptr, tween);
+    }
+    
+    
+    template<typename T>
+    static void remove(T* ptr){
         auto& items = instance().items_;
-        auto iter = std::find_if(items.begin(), items.end(), [ptr](const detail::TweenItem& item){
-            return item.ptr() == ptr;
+        auto iter = std::find_if(items.begin(), items.end(), [ptr](const std::shared_ptr<BaseTweenItem>& item){
+            return item->ptr() == ptr;
         });
         if(iter!=items.end()){
             items.erase(iter);
@@ -140,23 +113,57 @@ public:
     
     static void update(){
         auto& items = instance().items_;
-        auto result = std::remove_if(items.begin(), items.end(), [](const detail::TweenItem& item){
-            return item.complete();
+        auto result = std::remove_if(items.begin(), items.end(), [](const std::shared_ptr<BaseTweenItem>& item){
+            return item->complete();
         });
         items.erase(result, items.end());
-        std::for_each(items.begin(), items.end(), [](detail::TweenItem& item){
-            item.update(kTick);
+        std::for_each(items.begin(), items.end(), [](std::shared_ptr<BaseTweenItem>& item){
+            item->update(kTick);
         });
     }
     
 private:
+    
+    class BaseTweenItem {
+    public:
+        virtual ~BaseTweenItem() = default;
+        virtual void update(std::uint64_t tick) = 0;
+        virtual bool complete() const = 0;
+        virtual void* ptr() const = 0;
+    };
+    
+    template<typename T>
+    class TweenItemWrapper : public BaseTweenItem {
+    public:
+        TweenItemWrapper(T* ptr, const Tween<T>& tween)
+        : ptr_{ptr}, tween_{tween}{
+        }
+        
+        void update(std::uint64_t tick) override {
+            *ptr_ = tween_.step(16);
+        }
+        
+        bool complete() const override {
+            return tween_.progress() > 1.0 ? true : false;
+        }
+        
+        void* ptr() const override {
+            return ptr_;
+        }
+        
+    private:
+        Tween<T> tween_;
+        T* ptr_;
+        
+    };
+    
     static constexpr auto kTick = 16;
     Tweener(){}
     static Tweener& instance(){
         static Tweener instance{};
         return instance;
     }
-    std::vector<detail::TweenItem> items_;
+    std::vector<std::shared_ptr<BaseTweenItem>> items_;
 };
 
 } // namespace ofxTweeny
